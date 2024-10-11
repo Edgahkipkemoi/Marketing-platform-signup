ab66b95bf247b5d2c6d72dd0074c2d7893f43c1b
unknown <kipkemoiedgah@gmail.com>
2024-10-11 23:14:59 +0300

    navigation to other pages

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ loginButton.addEventListener('click', () => {
   loginPopup.style.display = 'block';
 });
 
+// Use jQuery to bind the click event to the login button
+$(document).ready(function() {
+  $('.btnLogin-popup').on('click', function(e) {
+    e.preventDefault(); // Prevent the default button behavior
+    $('.container').show(); // Show the popup container
+  });
+
+  // Add an event listener to the close icon to close the popup
+  $('.icon-close').on('click', function() {
+    $('.container').hide(); // Hide the popup container
+  });
+});
